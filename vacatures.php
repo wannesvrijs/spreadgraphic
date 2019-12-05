a6fb3f548d74b61af31ac3933817fc354976474a
gdm-1718-annepeet2 <annepeet2@student.arteveldehs.be>
2019-12-05 15:42:33 +0100

    test 22

--- vacatures.php
+++ vacatures.php
@@ -10,6 +10,7 @@ ShowMessages();
     <h1>Vacatures</h1>
     <p>anneleen was hier jow swag</p>
     <p>wannes was hier ook yolo lol xd rofl</p>
+    <p>tweede test </p>
 </div>
 
 <?php PrintNavBar(); ?>
