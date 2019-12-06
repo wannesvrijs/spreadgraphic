ec5f48bf2c6904bd40f63abfd674ec3f5aa2af29
wannes <wannes.vrijs@gmail.com>
2019-12-06 15:44:21 +0100

    profile_image_fix

--- login.php
+++ login.php
@@ -3,12 +3,13 @@ $login_form = true;
 require_once "lib/autoload.php";
 
 //redirect naar homepage als de gebruiker al ingelogd is
-if ( isset($_SESSION['use']) ) { $_SESSION["msg"][] = "U bent al ingelogd!"; header("Location: steden.php"); exit; }
+if ( isset($_SESSION['use']) ) { $_SESSION["msg"][] = "U bent al ingelogd!"; header("Location: index.php"); exit; }
 
 BasicHead();
 ShowMessages();
 ?>
 <body class="main loginbody">
+<?php PrintNavBar(); ?>
 
 <main class="container">
     <h1>Welcome back!</h1>
