7c082e30c16681862a15ff55d5b6d0a8e1f0936d
gdm-1718-annepeet2 <annepeet2@student.arteveldehs.be>
2019-12-09 21:01:23 +0100

    footer opmaak

--- lib/autoload.php
+++ lib/autoload.php
@@ -2,14 +2,16 @@
 session_start();
 $_SESSION["head_printed"] = false;
 
-require_once "pdo.php";                         //database functies
-require_once "view_functions.php";      //basic_head, load_template, replacecontent...
+require_once "pdo.php";                //database functies
+require_once "view_functions.php";     //basic_head, load_template, replacecontent...
 require_once "authorisation.php";      //controle login e.d.
 require_once "show_messages.php";
 
+// HEBBEN WIJ NIET NODIG OMDAT JE NIET PERSEE INGELOGD MOET ZIJN VOOR DE AFBEELDINGEN TE BEKIJKEN - ANNELEEN
+
 //redirect naar NO ACCESS pagina als de gebruiker niet ingelogd is en niet naar
 //de loginpagina gaat
-if ( ! isset($_SESSION['use']) AND ! $login_form AND ! $register_form AND ! $no_access)
-{
-    header("Location: /spreadgraphic/no_access.php");
-}
+//if ( ! isset($_SESSION['use']) AND ! $login_form AND ! $register_form AND ! $no_access)
+//{
+//   header("Location: /spreadgraphic/no_access.php");
+//}
--- no_access.php
+++ no_access.php
@@ -1,25 +1,28 @@
 <?php
-$no_access = true;
-require_once "lib/autoload.php";
 
-BasicHead();
+// HEBBEN WIJ NIET NODIG - ANNELEEN
+
+//$no_access = true;
+//require_once "lib/autoload.php";
+
+//BasicHead();
 
 ?>
 <body>
-<?php PrintNavBar(); ?>
+<?php //PrintNavBar(); ?>
 
-<div class="jumbotron text-center">
+<!--<div class="jumbotron text-center">
     <h1>Geen toegang!</h1>
     <p>Gelieve eerst in te loggen</p>
 </div>
 
-<div class="container">
+<div class="container"> -->
 
         <?php
-        print LoadTemplate("no_access");
+       // print LoadTemplate("no_access");
         ?>
-
+<!--
 </div>
 
 </body>
-</html>
+</html>-->
