aea458ed6742d32c61afa89aebcd37ffe9500d97
gdm-1718-annepeet2 <annepeet2@student.arteveldehs.be>
2019-12-11 15:25:10 +0100

    profile page

--- profile.php
+++ profile.php
@@ -1,20 +1,46 @@
-<?php
-require_once "lib/autoload.php";
-BasicHead();
-ShowMessages();
-?>
-
-<body id="page_artistpage">
-<?php PrintNavBar(); ?>
-
-<div class="container">
-
     <?php
-    print LoadTemplate("profile");
+    require_once "lib/autoload.php";
+    BasicHead();
+    ShowMessages();
     ?>
 
-</div>
-<?php PrintFooter(); ?>
+    <body id="page_profile">
+    <?php PrintNavBar(); ?>
+        <div class="container">
+            <div class="grid_artistpage">
+                <!-- profile picture -->
+                <div class="rondeimage">
+                    <a href="artistpage.php"><img src="img/graphic9.jpg" alt="person"></a>
+                </div>
+                <!-- name and caption -->
+                <div>
+                    <p class="name">John Smit</p>
+                    <p>Young designer form new york</p>
+                </div>
+                <!-- read more -->
+                <div>
+                    <a href="#">read more</a>
+                </div>
+                <!-- social media -->
+                <div>
+                    <a href="#" title="">
+                        <span class="fab fa-facebook-f"></span>
+                        <span class="sr-only">Facebook pagina</span>
+                    </a>
+                    <a href="#" title="">
+                        <span class="fab fa-instagram"></span>
+                        <span class="sr-only">Instagram pagina</span>
+                    </a>
+                </div>
+            </div>
+            <section>
+                <?php
+                print LoadTemplate("profile");
+                ?>
+            </section>
 
-</body>
+
+        </div>
+    <?php PrintFooter(); ?>
+    </body>
 </html>
