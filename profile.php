ba3f1224e977ebeb79dd2f593291575cf1f1d568
gdm-1718-annepeet2 <annepeet2@student.arteveldehs.be>
2019-12-11 18:52:02 +0100

    pop-up

--- profile.php
+++ profile.php
@@ -9,20 +9,42 @@ ShowMessages();
         <div class="container">
             <div class="grid_artistpage">
                 <!-- profile picture -->
-                <div class="rondeimage">
-                    <a href="artistpage.php"><img src="img/graphic9.jpg" alt="person"></a>
+                <div class="rondeimage profile_picture">
+                    <img src="img/graphic9.jpg" alt="person">
                 </div>
                 <!-- name and caption -->
-                <div>
+                <div class="profile_name">
                     <p class="name">John Smit</p>
                     <p>Young designer form new york</p>
                 </div>
                 <!-- read more -->
-                <div>
-                    <a href="#">read more</a>
+                <div class="profile_readmore">
+                    <a class="readmore" href="#popup_readmore">read more</a>
+                </div>
+
+                <div id="popup_readmore" class="overlay">
+                    <div class="popup">
+                        <div class="rondeimage">
+                            <img src="img/graphic2.jpg" alt="person">
+                        </div>
+                        <div class="popup_text">
+                            <div class="education">
+                            <h3>Education</h3>
+                            <p>Texas Art Institute</p>
+                        </div>
+                        <div class="about">
+                            <h3>About</h3>
+                            <p>I’m feeling so cool Top to the bottom, just cool Every little thing that I do Dammit, I’m feelin’ so cool, yeah Woke up feelin’ like a new James Dean I comb my hair like an old-school Sheen I’m feelin’ high like a late night summer of last year, yeah Standin’ there with the red dress on you A Killer Queen like a young Jane Fonda Is it me, or am I just havin’ a good year?</p>
+                        </div>
+
+                        </div>
+
+
+                        <a class="close" href="#">&times;</a>
+                    </div>
                 </div>
                 <!-- social media -->
-                <div>
+                <div class="profile_social">
                     <a href="#" title="">
                         <span class="fab fa-facebook-f"></span>
                         <span class="sr-only">Facebook pagina</span>
