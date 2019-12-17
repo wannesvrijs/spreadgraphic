2f316c658787e2c8193fd7abb6a31a259a439377
wannes <wannes.vrijs@gmail.com>
2019-12-17 18:26:14 +0100

    materials_fix + like_profile_fix

--- js/like.js
+++ js/like.js
@@ -1,17 +1,7 @@
 function like_add(gra_id) {
 	$.post('ajax/like_add.php',{gra_id:gra_id},function(data) {
 
-		$('#graphic_'+gra_id+'_likes').toggleClass("far fas");
+		$('.graphic_'+gra_id+'_likes').toggleClass("far fas");
 
-		if (data == 'success') {
-			like_get(gra_id);
-
-		}
-	});
-}
-
-function like_get(gra_id) {
-	$.post('ajax/like_get.php', {gra_id:gra_id},function(data) {
-		$('#graphic_'+gra_id+'_likes').text(data);
 	});
 }
