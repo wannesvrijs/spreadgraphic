d0b0f392fb2cc66a00b110f80b9289620ffd3af6
gdm-1718-annepeet2 <annepeet2@student.arteveldehs.be>
2019-12-17 18:53:29 +0100

    zoekbalk is klaar!!

--- index.php
+++ index.php
@@ -2,35 +2,6 @@
 require_once "lib/autoload.php";
 BasicHead();
 ShowMessages();
-
-// ZOEKBALK
-if(isset($_POST['search'])) {
-    $searchq = $_POST['search'];
-    $result = GetData ("SELECT * FROM graphic WHERE gra_tags LIKE '%$searchq%' ORDER BY gra_uploaddate DESC");
-    if(!$result) {
-       echo 'er loopt iets fout bij het zoeken';
-    } else {
-        echo 'als ik hier uitkom, klopt het!';
-    }
-    if($row['COUNT(*)'] == 0 ){
-        $output = 'There are no results';
-    } else {
-        while($row = mysqli_fetch_assoc($data_search)) {
-            $output .= ' wtf is dees';
-       }
-   }
-}
-
-
-//$timestamp = GetData("SELECT gra_uploaddate FROM graphic");
-//date(F d, Y)
-
-//$sql= "select gra_uploaddate from graphic where gra_id='" . $_GET['gra_id'] . "'";
-//$timestamp= GetData($sql);
-//$timestamp = date("F d, Y");
-//var_dump($timestamp);
-
-
 ?>
 
 <body id="page_index">
@@ -45,9 +16,33 @@ if(isset($_POST['search'])) {
     </div>
     <section>
         <?php
-        $data = GetData("select * from graphic inner join users on gra_use_id = use_id ORDER BY gra_uploaddate DESC");
-        $template = LoadTemplate("index");
-        print ReplaceContentindex( $data, $template);
+        if(isset($_POST['search'])) {
+            $searchq = $_POST['search'];
+            $result = GetData ("SELECT * FROM graphic WHERE gra_tags LIKE '%$searchq%' ORDER BY gra_uploaddate DESC");
+            if(!$result) {
+                $_POST["msg"][] = "There are no such tags!"; // dit werkt nog niet
+
+                $data = GetData("select * from graphic inner join users on gra_use_id = use_id ORDER BY gra_uploaddate DESC");
+                $template = LoadTemplate("index");
+                print ReplaceContentindex( $data, $template);
+            } else {
+                $data = GetData ("SELECT * FROM graphic inner join users on gra_use_id = use_id WHERE gra_tags LIKE '%$searchq%' ORDER BY gra_uploaddate DESC");
+                $template = LoadTemplate("index");
+                print ReplaceContentindex( $data, $template);
+            }
+            if($row['COUNT(*)'] == 0 ){
+                $output = 'There are no results';
+            } else {
+                while($row = mysqli_fetch_assoc($data_search)) {
+                    $output .= ' wtf is dees';
+                }
+            }
+        } else {
+            $data = GetData("select * from graphic inner join users on gra_use_id = use_id ORDER BY gra_uploaddate DESC");
+            $template = LoadTemplate("index");
+            print ReplaceContentindex( $data, $template);
+        }
+
         ?>
     </section>
 </div>
