a106172d37b1c35b1c4dc8b63a6b36d9c1c30aef
wannes <wannes.vrijs@gmail.com>
2019-12-17 09:26:08 +0100

    like_complete_fix

--- js/like.js
+++ js/like.js
@@ -1,5 +1,8 @@
 function like_add(gra_id) {
 	$.post('ajax/like_add.php',{gra_id:gra_id},function(data) {
+
+		$('#graphic_'+gra_id+'_likes').toggleClass("far fas");
+
 		if (data == 'success') {
 			like_get(gra_id);
 
