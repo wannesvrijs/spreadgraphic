d6200e34f620e6382687a3ad0028f7e38a00e7aa
gdm-1718-annepeet2 <annepeet2@student.arteveldehs.be>
2019-12-17 18:16:43 +0100

    zoekbalk php klopt enkel nog weergeven

--- topfive.php
+++ topfive.php
@@ -2,10 +2,6 @@
 require_once "lib/autoload.php";
 BasicHead();
 ShowMessages();
-
-
-
-
 ?>
     <body id="page_topfive">
     <?php PrintNavBar(); ?>
@@ -14,7 +10,7 @@ ShowMessages();
             <h1>These are our top five graphics this week. Get inspired…</h1>
 
             <?php
-            $data = GetData("select COUNT(like_id) AS aantal_likes, gra_image, gra_description, gra_tags, gra_uploaddate, use_caption, use_firstname, use_name, use_picture from graphic INNER JOIN likes ON like_gra_id = gra_id INNER JOIN users ON gra_use_id = use_id GROUP BY gra_id ORDER BY aantal_likes DESC LIMIT 5;");
+            $data = GetData("select COUNT(like_id) AS aantal_likes, gra_image, gra_description, gra_tags, gra_uploaddate, use_caption, use_firstname, use_name, use_picture, use_id from graphic INNER JOIN likes ON like_gra_id = gra_id INNER JOIN users ON gra_use_id = use_id GROUP BY gra_id ORDER BY aantal_likes DESC LIMIT 5;");
             $template = LoadTemplate("topfive");
             print ReplaceContent( $data, $template);
             ?>
