08712823dff9aebbbfaf679d9d42e46f25e330b9
gdm-1718-annepeet2 <annepeet2@student.arteveldehs.be>
2019-12-17 16:28:21 +0100

    php topfive pagina klaar

--- topfive.php
+++ topfive.php
@@ -3,6 +3,9 @@ require_once "lib/autoload.php";
 BasicHead();
 ShowMessages();
 
+
+
+
 ?>
     <body id="page_topfive">
     <?php PrintNavBar(); ?>
@@ -11,7 +14,8 @@ ShowMessages();
             <h1>These are our top five graphics this week. Get inspired…</h1>
 
             <?php
-            $data = GetData("select * from graphic LIMIT 5");
+            $data = GetData("select * from graphic inner join users on gra_use_id = use_id INNER JOIN likes ON like_gra_id = gra_id ORDER BY like_gra_id DESC
+LIMIT 5");
             $template = LoadTemplate("topfive");
             print ReplaceContent( $data, $template);
             ?>
