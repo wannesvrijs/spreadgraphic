841566a9d60f9b02a9025618c1e69d885b852c34
gdm-1718-annepeet2 <annepeet2@student.arteveldehs.be>
2019-12-17 12:55:41 +0100

    ff pushen

--- profile.php
+++ profile.php
@@ -13,18 +13,14 @@ ShowMessages();
                 } else {
                     $data_id = $_GET['id'];
                 }
-
                 $data = GetData("select * from users where use_id='" . $data_id . "'");
                 $template = LoadTemplate("profile_top");
                 print ReplaceContent( $data, $template);
             ?>
             <section>
             <?php
-                if (IsMe()) echo"<a href='add_graphic.php' class='profile_underline add_graphic_here'>
-                                     <span class='fas fa-plus'></span>
-                                 </a>";
-
-                $data = GetData("select * from graphic where gra_use_id='" . $data_id . "'");
+                if (IsMe()) echo"<a href='add_graphic.php' class='profile_underline add_graphic_here'><span class='fas fa-plus'></span></a>";
+                $data = GetData("select * from graphic inner join users on gra_use_id = use_id where gra_use_id='" . $data_id . "' ORDER BY gra_uploaddate DESC");
                 $template = LoadTemplate("index");
                 print ReplaceContent( $data, $template);
             ?>
