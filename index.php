dddf7b0e9929758854a26ed04b65adf83f6e95f5
gdm-1718-annepeet2 <annepeet2@student.arteveldehs.be>
2019-12-18 19:20:47 +0100

    go to top knop

--- index.php
+++ index.php
@@ -45,6 +45,10 @@ ShowMessages();
         }
         ?>
     </section>
+    <!-- go to top knop -->
+    <a href="#" id="goToTop">
+        <span class="fa fa-arrow-circle-up"></span>
+    </a>
 </div>
 
 <?php PrintFooter(); ?>
